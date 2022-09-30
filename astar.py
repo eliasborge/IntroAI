79bd87b688b19decd11819e4583af3c31f7dc2e4
Elias Borge Svinø <eliasbosv@gmail.com>
2022-09-30 17:01:28 +0200

    Finalize project

--- astar.py
+++ astar.py
@@ -5,124 +5,118 @@ from numpy import true_divide
 import Map
 
 class Node:
-    def __init__(self, cost=-1, parent = None, position = [0,0]):
+    def __init__(self, position:list):
         self.position= position
-        self.cost = cost
-        self.parent = parent
+        self.cost = -1
+        self.parent = None
         self.children = []
         self.gn = 0
         self.hn = 0
         self.fn = 0
 
-
-    def toString(self):
-        return str(self.position) + " f: " + str(self.f) + " c: "+ str(self.cost)
-
-    def checkIfSamePos(self,otherNode):
-        return self.position == otherNode.position
-    
-    def checkF(self,target):
-        dx = abs(target.position[0] - self.position[0])
-        dy = abs(target.position[1] - self.position[1])
+    def checkF(self,targetNode):
+        dx = abs(targetNode.position[0] - self.position[0])
+        dy = abs(targetNode.position[1] - self.position[1])
 
         self.hn = dx + dy
         self.fn = self.gn + self.hn
-        
 
 
-def check_if_neighbours(a: Node, b: Node):
-    #Check if they are left-right neighbours
-    if(a.position[1] == b.position[1]):
-        if (a.position[0] == b.position[1] + 1) or (a.position[0] == b.position[0]-1):
-            return True
-    
-    #Check if they are up-down neighbours: 
-    if(a.position[0] == b.position[0]):
-        if(a.position[1] == b.position[0] +1) or (a.position[1] == b.position[1] -1):
-            return True
+    def find_children(self, map:Map.Map_Obj):
+        children = []
+        up = [self.position[0]-1,self.position[1]]
+        down = [self.position[0]+1,self.position[1]]
+        left = [self.position[0],self.position[1]+1]
+        right = [self.position[0],self.position[1]-1]
 
-    # if neither of them run through.
-    return False
+        dir = [up,down,left,right]
+        for d in dir:
+            if map.get_cell_value(d) > -1:
+                children.append(Node(d))
 
-def inform_of_better_paths(parent:Node):
+        return children
+
+def inform_of_better_paths(parent:Node, map: Map.Map_Obj):
     for child in parent.children:
-        if parent.gn + child.cost < child.gn:
+        if parent.gn + map.get_cell_value(child.position) < child.gn:
             child.parent = parent
-            child.gn = parent.gn + child.cost
-            inform_of_better_paths(child)
+            attach_and_evaluate(child,parent,map)
+            inform_of_better_paths(child,map)
+
+def attach_and_evaluate(child:Node,parent:Node,map:Map.Map_Obj):
+    child.parent = parent
+    child.gn = parent.gn + map.get_cell_value(child.position)
+    child.checkF(Node(map.get_end_goal_pos()))
+
+def tracePath(node:Node,map:Map.Map_Obj):
+    map.set_cell_value(map.get_goal_pos(),6)
+    while node.parent is not None:
+        if node.position == map.get_goal_pos:
+            break
+        map.set_cell_value(node.position,5)
+        node = node.parent
+    map.set_cell_value(map.get_end_goal_pos(),7)
+    map.show_map()
+    
+
+    
+def check_if_child_is_created(child:Node,list1:list[Node],list2:list[Node]):
+    for node in list1:
+        if node.position == child.position:
+            return node 
+
+    for node in list2:
+        if node.position == child.position:
+            return node
+    return child
 
 
-def aStar(start: Node, target: Node, nodes: List[Node]):
+def aStar(map:Map.Map_Obj):
     #setup
+    print("A* Algorithm activated")
     opened = []
     closed = []
-    opened.append(start)
+    node = Node(map.get_start_pos())
+    opened.append(node)
 
     #Agenda loop
-    while True: 
+    while node.position != map.get_goal_pos(): 
         if len(opened) == 0:
-            return False 
+            print("Stopped. A* failed")
+            return current_node 
         
         current_node = opened.pop()
         closed.append(current_node)
 
-        if(current_node == target):
-            return True
-        
-        #Has to find all children of the current node
 
-        for node in nodes:
-            if check_if_neighbours(current_node,node):
-                current_node.children.append(node)
+        if(current_node.position == map.get_goal_pos()):
+            print("Works!")
+            return current_node
         
-        for child in current_node.children:
-            if child not in opened and child not in closed:
-                child.parent = current_node
-                child.gn = child.parent.gn + child.cost
-                child.checkF(target)
-                opened.append(child)
-            elif current_node.gn + child.cist < child.gn:
-                child.parent = current_node
-                child.gn = child.parent.gn + child.cost
-                child.checkF(target)
-                if child in closed:
-                    inform_of_better_paths(child)
+        #Have to find all children of the current node
+        children = current_node.find_children(map)
+
+        for child in children:
+            new_child = check_if_child_is_created(child,opened,closed)
+            if new_child not in opened and new_child not in closed:
+                attach_and_evaluate(new_child,current_node,map)
+                opened.append(new_child)
+                opened.sort(key=lambda y: y.fn, reverse=True)
+            elif current_node.gn + map.get_cell_value(new_child.position) < new_child.gn:
+                attach_and_evaluate(new_child,current_node,map)
+                if new_child in closed:
+                    inform_of_better_paths(new_child,map)
+            
 
 
 
 def main():
-    x=0
-    # #Change this variable based on task
-    task = 1
-    print("hei")
 
-    map = Map.Map_Obj(task = task)
-    start = map.get_start_pos()
-    target = map.get_end_goal_pos()
-    path = map.fill_critical_positions(task=task)[3]
-    grid = map.read_map(path)[0]
-    startNode = None
-    goalNode = None
-    nodes = []
-
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            node = Node(cost=grid[y][x],position=[y,x]) 
-            nodes.append(node)
-            if node.position == start:
-                y=0
-
-    # Det her er et forsøk på implementasjon inspirert av en annen greie
-    # Men det er ikke helt ferdig. 
-    # Alt over er sånn standard implementasjon av selve algoritmen 
-    # og nodene.
-
+    map = Map.Map_Obj(4) # <--- Change this input number to change task
+    x = aStar(map)
+    tracePath(x,map)
 
     
-
-
-
-x=0
 main()
 
 
